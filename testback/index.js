457ef912c3fe28ec5b873fdc5e5baa0205a2ca36
subham348 <subhammahato348@gmail.com>
2023-06-06 00:50:42 +0530

    nodemon and listening to a port

--- testback/index.js
+++ testback/index.js
@@ -1,3 +1,21 @@
 const express = require("express");
 
 const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  return res.send("Hello there!!");
+});
+
+app.get("/login", (req, res) => {
+  return res.send("You are visiting login route");
+});
+
+app.get("/signup", (req, res) => {
+  return res.send("You are visiting signup route");
+});
+
+app.listen(port, () => {
+  console.log("Server is up and running...");
+});
