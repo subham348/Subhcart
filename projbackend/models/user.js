2c4f293f70c18b3876f7a0d254fd3e9ead73c363
subham348 <subhammahato348@gmail.com>
2023-06-13 01:45:51 +0530

    Created userSchema and hash password storing

---
+++ projbackend/models/user.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+const { createHmac } = await import("node:crypto");
+import { v4 as uuidv4 } from "uuid";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userinfo: {
+    type: String,
+  },
+  encry_password: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+userSchema
+  .virtuals("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.method = {
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encry_password;
+  },
+
+  securePassword: function (plainPassword) {
+    if (!plainPassword) return "";
+    try {
+      return createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
