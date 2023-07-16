d129dc73f313d5bf829be059a6e6991d01c8d8ea
subham348 <subhammahato348@gmail.com>
2023-07-17 02:06:25 +0530

    Playing around middlewares

--- testback/index.js
+++ testback/index.js
@@ -16,6 +16,22 @@ app.get("/signup", (req, res) => {
   return res.send("You are visiting signup route");
 });
 
+const isLoggedIn = (req, res, next) => {
+  console.log("isLoggedIn is running..");
+  next();
+};
+
+const isAdmin = (req, res, next) => {
+  console.log("isAdmin is running..");
+  next();
+};
+
+const admin = (req, res) => {
+  return res.send("This is Admin");
+};
+
+app.get("/admin", isLoggedIn, isAdmin, admin);
+
 app.listen(port, () => {
   console.log("Server is up and running...");
 });
